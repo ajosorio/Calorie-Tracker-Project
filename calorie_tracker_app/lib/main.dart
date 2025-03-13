cfb4e6da929ec7ebcfff46d67de1834ae634972c
Alejandro Osorio <aosorio0498@gmail.com>
2025-03-13 19:43:34 -0400

    Made About Screen and Button

--- calorie_tracker_app/lib/main.dart
+++ calorie_tracker_app/lib/main.dart
@@ -13,7 +13,49 @@ class HomeScreen extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(title: Text("Macro Mate")),
         body: Center(
-          child: Text('Login screen stuff...'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Login Screen stuff..."),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  runApp(const AboutScreen());
+                },
+                child: Text("Go to About  Screen"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class AboutScreen extends StatelessWidget {
+  const AboutScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("This page was made by Alejandro"),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    runApp(const HomeScreen());
+                  },
+                  child: Text("Go to Home Screen"))
+            ],
+          ),
         ),
       ),
     );
