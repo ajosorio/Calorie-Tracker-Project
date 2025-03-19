9e3b91268112b0f10e3f3adc1aad4278146b4413
David S <161085018+dstraco@users.noreply.github.com>
2025-03-18 20:35:37 -0400

    Created login screen and implemented login feature. Sign up feature and screen is in progress. Improved route navigation between the two screens.

--- calorie_tracker_app/lib/main.dart
+++ calorie_tracker_app/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
-  runApp(const HomeScreen());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MaterialApp(title: "Firebase Example", home: LoginScreen()));
 }
 
 class HomeScreen extends StatelessWidget {
@@ -16,16 +21,37 @@ class HomeScreen extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text("Login Screen stuff..."),
+              Text("Welcome ${FirebaseAuth.instance.currentUser?.email}"),
               SizedBox(
                 height: 20,
               ),
               ElevatedButton(
                 onPressed: () {
-                  runApp(const AboutScreen());
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const AboutScreen()
+                        // runApp(const AboutScreen());
+                        ),
+                  );
                 },
-                child: Text("Go to About  Screen"),
+                child: Text("Go to About Screen"),
               ),
+              ElevatedButton(
+                onPressed: () {
+                  // signOut() doesn't return anything, so we don't need to await
+                  // for it to finish unless we really want to.
+                  FirebaseAuth.instance.signOut();
+
+                  // This navigator call clears the Navigation stack and takes
+                  // them to the login screen because we don't want users
+                  // "going back" in our app after they log out.
+                  Navigator.of(context).pushAndRemoveUntil(
+                      MaterialPageRoute(
+                          builder: (context) => const LoginScreen()),
+                      (route) => false);
+                },
+                child: const Text("Logout"),
+              )
             ],
           ),
         ),
@@ -51,7 +77,7 @@ class AboutScreen extends StatelessWidget {
               ),
               ElevatedButton(
                   onPressed: () {
-                    runApp(const HomeScreen());
+                    Navigator.pop(context);
                   },
                   child: Text("Go to Home Screen"))
             ],
@@ -59,5 +85,238 @@ class AboutScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  String? email;
+  String? password;
+  String? error;
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                  decoration:
+                      const InputDecoration(hintText: 'Enter your email'),
+                  maxLength: 64,
+                  onChanged: (value) => email = value,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null; // Returning null means "no issues"
+                  }),
+              TextFormField(
+                  decoration:
+                      const InputDecoration(hintText: "Enter a password"),
+                  obscureText: true,
+                  onChanged: (value) => password = value,
+                  validator: (value) {
+                    if (value == null || value.length < 8) {
+                      return 'Your password must contain at least 8 characters.';
+                    }
+                    return null; // Returning null means "no issues"
+                  }),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                  child: const Text('Login'),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      tryLogin();
+                    }
+                  }),
+              if (error != null)
+                Text(
+                  "Error: $error",
+                  style: TextStyle(color: Colors.red[800], fontSize: 12),
+                ),
+              ElevatedButton(
+                  child: const Text('Sign up'),
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => const SignupScreen()));
+                  })
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void tryLogin() async {
+    try {
+      // The await keyword blocks execution to wait for
+      // signInWithEmailAndPassword to complete its asynchronous execution and
+      // return a result.
+      //
+      // FirebaseAuth will raise an exception if the email or password
+      // are determined to be invalid, e.g., the email doesn't exist.
+      final credential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email!, password: password!);
+      print("Logged in ${credential.user}");
+      error = null; // clear the error message if exists.
+      setState(() {}); // Trigger a rebuild
+
+      // We need this next check to use the Navigator in an async method.
+      // It basically makes sure LoginScreen is still visible.
+      if (!mounted) return;
+
+      // pop the navigation stack so people cannot "go back" to the login screen
+      // after logging in.
+      Navigator.of(context).pop();
+      // Now go to the HomeScreen.
+      Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) => const HomeScreen(),
+      ));
+    } on FirebaseAuthException catch (e) {
+      // Exceptions are raised if the Firebase Auth service
+      // encounters an error. We need to display these to the user.
+      if (e.code == 'user-not-found') {
+        error = 'No user found for that email.';
+      } else if (e.code == 'wrong-password') {
+        error = 'Wrong password provided for that user.';
+      } else {
+        error = 'An error occurred: ${e.message}';
+      }
+
+      // Call setState to redraw the widget, which will display
+      // the updated error text.
+      setState(() {});
+    }
+  }
+}
+
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({super.key});
+
+  @override
+  State<SignupScreen> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  String? email;
+  String? password;
+  String? error;
+  final _formKey = GlobalKey<FormState>();
+  @override
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                  decoration:
+                      const InputDecoration(hintText: 'Enter your email'),
+                  maxLength: 64,
+                  onChanged: (value) => email = value,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null; // Returning null means "no issues"
+                  }),
+              TextFormField(
+                  decoration:
+                      const InputDecoration(hintText: "Enter a password"),
+                  obscureText: true,
+                  onChanged: (value) => password = value,
+                  validator: (value) {
+                    if (value == null || value.length < 8) {
+                      return 'Your password must contain at least 8 characters.';
+                    }
+                    return null; // Returning null means "no issues"
+                  }),
+              TextFormField(
+                  decoration:
+                      const InputDecoration(hintText: "Confirm password"),
+                  obscureText: true,
+                  onChanged: (value) => password = value,
+                  validator: (value) {
+                    if (value == null || value.length < 8) {
+                      return 'Your password must contain at least 8 characters.';
+                    }
+                    return null; // Returning null means "no issues"
+                  }),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                  child: const Text('Sign up'),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      trySignup();
+                    }
+                  }),
+              if (error != null)
+                Text(
+                  "Error: $error",
+                  style: TextStyle(color: Colors.red[800], fontSize: 12),
+                )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void trySignup() async {
+    try {
+      // The await keyword blocks execution to wait for
+      // signInWithEmailAndPassword to complete its asynchronous execution and
+      // return a result.
+      //
+      // FirebaseAuth will raise an exception if the email or password
+      // are determined to be invalid, e.g., the email doesn't exist.
+      final credential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email!, password: password!);
+      print("Created account ${credential.user}");
+      error = null; // clear the error message if exists.
+      setState(() {}); // Trigger a rebuild
+
+      // We need this next check to use the Navigator in an async method.
+      // It basically makes sure LoginScreen is still visible.
+      if (!mounted) return;
+
+      // pop the navigation stack so people cannot "go back" to the login screen
+      // after logging in.
+      Navigator.of(context).pop();
+      // Now go to the HomeScreen.
+      Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) => const HomeScreen(),
+      ));
+    } on FirebaseAuthException catch (e) {
+      // Exceptions are raised if the Firebase Auth service
+      // encounters an error. We need to display these to the user.
+      if (e.code == 'user-not-found') {
+        error = 'No user found for that email.';
+      } else if (e.code == 'wrong-password') {
+        error = 'Wrong password provided for that user.';
+      } else {
+        error = 'An error occurred: ${e.message}';
+      }
+
+      // Call setState to redraw the widget, which will display
+      // the updated error text.
+      setState(() {});
+    }
   }
 }
