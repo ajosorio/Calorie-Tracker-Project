622237baa66970255eba6aee6bb1e137d2e81e3d
David S <161085018+dstraco@users.noreply.github.com>
2025-03-20 16:39:14 -0400

    created meal class (almost complete) created food class (shell)

---
+++ calorie_tracker_app/lib/food.dart
@@ -0,0 +1,3 @@
+class Food {
+  /// A class to model food objects, contains information relating to nutrient values
+}
--- calorie_tracker_app/lib/main.dart
+++ calorie_tracker_app/lib/main.dart
@@ -3,6 +3,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+import 'package:calorie_tracker_app/food.dart';
+import 'package:calorie_tracker_app/meal.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
